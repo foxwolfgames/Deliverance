d09707c85e47cd5fb89af0a39e655d74054f1dc5
jstnf <justin@justinf.dev>
2024-09-09 22:29:32 -0500

    feat: Game manager singleton

---
+++ Assets/_Project/Scripts/DeliveranceGameManager.cs
@@ -0,0 +1,28 @@
+using FWGameLib.Common.AudioSystem;
+using FWGameLib.InProject.EventSystem;
+using UnityEngine;
+
+namespace Deliverance
+{
+    public class DeliveranceGameManager : MonoBehaviour
+    {
+        public static DeliveranceGameManager Instance;
+        public AudioManager Audio;
+        public EventRegister EventRegister;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+                // Initialize event register
+                EventRegister = new();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}

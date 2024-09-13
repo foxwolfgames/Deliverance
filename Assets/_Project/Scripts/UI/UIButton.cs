bcf18c00494b500a4dd9ebd945f12fa612d51447
jstnf <justin@justinf.dev>
2024-09-13 03:49:57 -0500

    feat: Fancy looking button

--- Assets/_Project/Scripts/UI/UIButton.cs
+++ Assets/_Project/Scripts/UI/UIButton.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using FWGameLib.Common.EventSystem;
 using FWGameLib.InProject.AudioSystem;
+using TMPro;
 using UnityEngine;
 
 namespace Deliverance.UI
@@ -8,11 +9,39 @@ namespace Deliverance.UI
     public class UIButton : MonoBehaviour
     {
         [SerializeField] public string eventName;
+        [SerializeField] public TextMeshProUGUI text;
+        [SerializeField] public Color textUnselectedColor;
+        [SerializeField] public Color textSelectedColor;
+        [SerializeField] private bool isMouseOver;
+        [SerializeField] private Color currentColor;
 
         public void OnClick()
         {
             new UIButtonPressEvent(eventName).Invoke();
             DeliveranceGameManager.Instance.Audio.PlaySound(Sounds.SFX_UI_BUTTON_CLICK);
+        }
+
+        public void MouseOn()
+        {
+            isMouseOver = true;
+        }
+
+        public void MouseOff()
+        {
+            isMouseOver = false;
+        }
+
+        void Awake()
+        {
+            currentColor = textUnselectedColor;
+            text.color = currentColor;
+        }
+
+        void Update()
+        {
+            Color targetColor = isMouseOver ? textSelectedColor : textUnselectedColor;
+            currentColor = Color.Lerp(currentColor, targetColor, Time.deltaTime * 10);
+            text.color = currentColor;
         }
     }
 
